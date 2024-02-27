6958182e897501ba515a045c4749676eaa84d6e6
RustamAleev2023 <ridermix@yandex.ru>
2024-02-27 20:16:16 +0200

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,53 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "test.txt";
-        String fullName = "";
-        String dirName = System.getProperty("user.dir");
-        fullName = dirName + File.separator + fileName;
-        System.out.println("File path: " + fullName);
-        File file = new File(fullName);
-
-        if(!file.exists()) {
+//        String fileName = "test.txt";
+//        String fullName = "";
+//        String dirName = System.getProperty("user.dir");
+//        fullName = dirName + File.separator + fileName;
+//        String dirname = dirName + "/tmp/user/java/bin";
+//        File d = new File(dirname);
+//        d.mkdirs();
+//        System.out.println("File path: " + fullName);
+//        File file = new File(fullName);
+//
+//        if(!file.exists()) {
+//            try {
+//                if (file.createNewFile()) {
+//                    System.out.println("File created!");
+//                } else {
+//                    System.out.println("Something wrong");
+//                }
+//            } catch (IOException e) {
+//                Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, e);
+//            }
+//        } else {
+//            System.out.println("File already exists!");
+//        }
+        InputStream in = null;
+        OutputStream out = null;
+        byte[] buffer = null;
+        try {
+           in = new FileInputStream(new File("test.txt"));
+           buffer = new byte[in.available()];
+           in.read(buffer);
+           File file = new File("outputFile.tmp");
+           out = new FileOutputStream(file);
+           out.write(buffer);
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, e);
+        } catch (IOException e) {
+            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
             try {
-                if (file.createNewFile()) {
-                    System.out.println("File created!");
-                } else {
-                    System.out.println("Something wrong");
-                }
+               in.close();
+               out.close();
             } catch (IOException e) {
                 Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, e);
             }
-        } else {
-            System.out.println("File already exists!");
         }
     }
 }
